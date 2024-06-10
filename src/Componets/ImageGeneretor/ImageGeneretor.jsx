258e6d1d5e366bd91198b877e0a518c72f1ede94
mehedifiz <mehedi.shanto.contract@gamil.com>
2024-06-10 22:53:46 +0600

    d

--- src/Componets/ImageGeneretor/ImageGeneretor.jsx
+++ src/Componets/ImageGeneretor/ImageGeneretor.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './ImageGeneretor.css'
 
 import defaultImage from '../Assets/image.png'
 
 const ImageGeneretor = () => {
+const [image , setImage] = useState('/');
+
+let inputRef = useRef(null);
+
+const imageGeneretor = async =>{
+    if(inputRef.current.value===''){
+        return 0;
+    }
+    const response = await fetch ()
+}
+
+
     return (
         <div className='ai-image-gen'>
 
             <div className='header'> Ai image <span>Generaor</span>  </div>
 
             <div className="img-loading">
-                <div className="image"><img src={defaultImage} alt="" /></div>
+                <div className="image"><img src={image==='/'? defaultImage : image} alt="" /></div>
+
+                <div className="search-box">
+                    <input ref={inputRef} type="text" className='search-input' placeholder='What is on you imagination ? ' />
+
+                    <div className="btn">Generete</div>
+                </div>
             </div>
           
         </div>
